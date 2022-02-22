1c24d7d973594c4157eea130857e337c35c1488c
Naima Hassan <naima.hassan434@gmail.com>
2022-02-22 16:54:21 +0000

    Rendering css, images, svg

--- index.js
+++ index.js
@@ -22,6 +22,18 @@ const server = http.createServer(async (req, res) => {
     const html = await fs.readFile("./views/overview.html", "utf-8");
     res.writeHead(200, { "Content-Type": "text/html" });
     res.end(html);
+  } else if (/\.(png)$/i.test(req.url)) {
+    const image = await fs.readFile(`./public/images/${req.url.slice(1)}`);
+    res.writeHead(200, { "Content-Type": "image/png" });
+    res.end(image);
+  } else if (/\.(svg)$/i.test(req.url)) {
+    const svg = await fs.readFile(`./public/images/icons.svg`);
+    res.writeHead(200, { "Content-Type": "image/svg+xml" });
+    res.end(svg);
+  } else if (/\.(css)$/i.test(req.url)) {
+    const css = await fs.readFile(`./public/css/index.css`);
+    res.writeHead(200, { "Content-Type": "text/css" });
+    res.end(css);
   } else {
     res.writeHead(404, { "Content-Type": "text/html" });
     res.end("<div> <h2>File Not Found</h2> </div>");
