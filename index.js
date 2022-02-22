1da31214562769f61425ace84da3a8cff31afaa9
Naima Hassan <naima.hassan434@gmail.com>
2022-02-22 16:12:36 +0000

    Parsing URL & Query String

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 const http = require("http");
+const url = require("url");
 
 const server = http.createServer((req, res) => {
-  console.log("Server running");
+  if (req.url === "/favicon.ico") return;
+  console.log(req.url);
+  console.log(req.headers);
+  //parsing url
+  const myUrl = new URL(req.url, `http://${req.headers.host}/`); //dynamic host name
+
+  const pathname = myUrl.pathname;
+  const id = myUrl.searchParams.get("id");
+  console.log(pathname, id);
+
   res.writeHead(200, { "Content-Type": "text/html" });
   res.end("<h1>Welcome to the bicycle shop!</h1>");
 });
